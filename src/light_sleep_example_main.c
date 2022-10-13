525a948c11b3cd6bfe6eaca905ce71306cf29f84
NRollo <niels@pryds-rolsted.dk>
2022-10-13 13:39:56 +0200

    first commit

---
+++ src/light_sleep_example_main.c
@@ -0,0 +1,95 @@
+/* Light sleep example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_sleep.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "esp_timer.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "addr_from_stdin.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "esp_wifi.h"
+
+/* Most development boards have "boot" button attached to GPIO0.
+ * You can also change this to another pin.
+ */
+#if CONFIG_IDF_TARGET_ESP32C3
+#define BUTTON_GPIO_NUM_DEFAULT     9
+#else
+#define BUTTON_GPIO_NUM_DEFAULT     0
+#endif
+
+/* "Boot" button is active low */
+#define BUTTON_WAKEUP_LEVEL_DEFAULT     0
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    
+       /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_set_ps(WIFI_PS_NONE));
+
+    while (true) {
+        /* Wake up in 2 seconds, or when button is pressed */
+        esp_sleep_enable_timer_wakeup(10000000);
+        printf("Entering light sleep\n");
+
+        esp_wifi_stop();
+        /* To make sure the complete line is printed before entering sleep mode,
+         * need to wait until UART TX FIFO is empty:
+         */
+        uart_wait_tx_idle_polling(CONFIG_ESP_CONSOLE_UART_NUM);
+
+        /* Get timestamp before entering sleep */
+        int64_t t_before_us = esp_timer_get_time();
+        
+        /* Enter sleep mode */
+        esp_light_sleep_start();
+        /* Execution continues here after wakeup */
+        esp_wifi_start();
+        /* Get timestamp after waking up from sleep */
+        int64_t t_after_us = esp_timer_get_time();
+
+        /* Determine wake up reason */
+        const char* wakeup_reason;
+        switch (esp_sleep_get_wakeup_cause()) {
+            case ESP_SLEEP_WAKEUP_TIMER:
+                wakeup_reason = "timer";
+                break;
+            case ESP_SLEEP_WAKEUP_GPIO:
+                wakeup_reason = "pin";
+                break;
+            default:
+                wakeup_reason = "other";
+                break;
+        }
+
+        printf("Returned from light sleep, reason: %s, t=%lld ms, slept for %lld ms\n",
+                wakeup_reason, t_after_us / 1000, (t_after_us - t_before_us) / 1000);
+    }
+
+}
