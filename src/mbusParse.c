171371bed27b23b5299476c6584f74238280c352
NRollo <niels@pryds-rolsted.dk>
2022-10-24 12:59:02 +0200

    cleanup

--- src/mbusParse.c
+++ src/mbusParse.c
@@ -4,22 +4,16 @@
 #include "mbusparser.h"
 #include "esp_log.h"
 
-static const char *TAG = "ParseMbus";
-
 extern struct MeterData parseMbusFrame(unsigned char *frame, int fLen);
 extern void mbus_data_tm_decode(struct tm *t, unsigned char *t_data, size_t t_data_size);
 extern int PublishMqtt(struct MeterData md);
 
 int mbusParseFrame(unsigned char *vframe, int vframeLen){
-    char buf[25] = {"\000"};
     unsigned char frame_vec[vframeLen];
     memcpy(frame_vec, vframe, vframeLen);
 
     struct MeterData md = parseMbusFrame(frame_vec, vframeLen);
 
-    ESP_LOGI(TAG, "activeImportWh: %d Valid: %d", md.activeImportWh, md.activeImportWhValid);
-    strftime(buf, 25, "%Y-%m-%d %H:%M:%S", &md.DateTime);
-    ESP_LOGI(TAG, "Timestamp: %s", buf);
     PublishMqtt(md);
     return 0;
 }
