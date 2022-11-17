f2b9c5cf5c90fe17d60e9f12f0130b8f11b466c2
NRollo <niels@pryds-rolsted.dk>
2022-11-17 15:59:17 +0100

    Iinitial commit

---
+++ src/Mqtt.c
@@ -0,0 +1,113 @@
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+static const char *TAG = "MQTT";
+#define CONFIG_BROKER_URL "192.168.0.71"
+
+extern void GetCurrentTemp(float *temp);
+esp_mqtt_client_handle_t MqttClient = NULL;
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+/*
+ * @brief Event handler registered to receive MQTT events
+ *
+ *  This function is called by the MQTT client event loop.
+ *
+ * @param handler_args user data registered to the event.
+ * @param base Event base for the handler(always MQTT Base in this example).
+ * @param event_id The id for the received event.
+ * @param event_data The data for the event, esp_mqtt_event_handle_t.
+ */
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    /*
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    */
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        esp_mqtt_client_reconnect(event->client);
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC= %.*s\r\n", event->topic_len, event->topic);
+        printf("DATA= %.*s\r\n", event->data_len, event->data);
+        break;
+
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .client_id = "TempSensor",
+        .username = "IOT",
+        .password = "Mondeo",
+        .port = 1883,
+        .host = CONFIG_BROKER_URL,
+    };
+
+    MqttClient = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(MqttClient, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(MqttClient);
+}
+
+void PublishMqttTask(void *pvParameters) {
+    char buf[30] = "\000";
+    int msg_id = 0;
+    float temp = 0.0;
+
+    mqtt_app_start();
+
+    while(1) {
+        GetCurrentTemp(&temp);
+        sprintf(buf, "%.1f", temp);
+        msg_id = esp_mqtt_client_publish(MqttClient, "/Temp/Sensor1", buf, 0, 1, 1);
+        if (msg_id < 0) {
+            ESP_LOGE(TAG, "Sensor not published: %d", msg_id);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(30000));
+    }
+}
+
---
+++ src/tcp_client.c
@@ -0,0 +1,71 @@
+/* BSD Socket API Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "addr_from_stdin.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "driver/gpio.h"
+
+extern void GetTempTask(void *data);
+extern void PublishMqttTask(void *pvParameters);
+
+SemaphoreHandle_t mtexCurrentTemp = NULL;
+
+// The error LED
+#define ERROR_LED GPIO_NUM_2
+#define LED_ON 1
+#define LED_OFF 0
+
+// The FAN output
+#define FAN GPIO_NUM_4
+#define FAN_ON 0
+#define FAN_OFF 1
+
+void app_main(void)
+{
+    // Configure the Fan to GPIO #4 and ERROR LED to GPIO #2
+    gpio_config_t IOpin = {
+                            .mode = GPIO_MODE_OUTPUT,
+                            .pin_bit_mask = (1ULL<<FAN) | (1ULL<<ERROR_LED),
+                            .pull_down_en = 0,
+                            .pull_up_en = 0,
+                            .intr_type = GPIO_PIN_INTR_DISABLE,
+                          };
+    // Make sure the FAN is off and the ERROR LED is on before the GPIO is configured
+    gpio_set_level(FAN, FAN_OFF);
+    gpio_set_level(ERROR_LED, LED_ON);
+    gpio_config(&IOpin);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+	configASSERT(mtexCurrentTemp = xSemaphoreCreateMutex());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(GetTempTask, "Temperature", 3000, NULL, 5, NULL);
+    xTaskCreate(PublishMqttTask, "mqtt_client", 4096, NULL, 3, NULL);
+
+    gpio_set_level(ERROR_LED, LED_OFF);
+}
