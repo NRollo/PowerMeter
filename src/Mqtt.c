bce2ba71e55e4858e01c160f8a5f5e9785fad42b
NRollo <niels@pryds-rolsted.dk>
2022-12-06 00:51:55 +0100

    Acer

--- src/Mqtt.c
+++ src/Mqtt.c
@@ -1,8 +1,13 @@
 #include "esp_log.h"
 #include "mqtt_client.h"
+#include "driver/gpio.h"
 
 static const char *TAG = "MQTT";
 #define CONFIG_BROKER_URL "192.168.0.71"
+#define ERROR_LED GPIO_NUM_2
+#define LED_ON 1
+#define LED_OFF 0
+
 
 extern void GetCurrentTemp(float *temp);
 esp_mqtt_client_handle_t MqttClient = NULL;
@@ -52,6 +57,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
     case MQTT_EVENT_PUBLISHED:
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        gpio_set_level(ERROR_LED, LED_OFF);
         break;
 
     case MQTT_EVENT_DATA:
@@ -102,7 +108,8 @@ void PublishMqttTask(void *pvParameters) {
     while(1) {
         GetCurrentTemp(&temp);
         sprintf(buf, "%.1f", temp);
-        msg_id = esp_mqtt_client_publish(MqttClient, "/Temp/Sensor1", buf, 0, 1, 1);
+        gpio_set_level(ERROR_LED, LED_ON);
+        msg_id = esp_mqtt_client_publish(MqttClient, "/Temp/Skur", buf, 0, 1, 1);
         if (msg_id < 0) {
             ESP_LOGE(TAG, "Sensor not published: %d", msg_id);
         }
